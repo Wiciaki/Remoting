447b8e0df971b932472fc8a3b35f6a92a12c5e7e
spark <wicia@WICIAKI>
2017-01-17 21:10:07 +0100

    Up

--- Uninstall/Program.cs
+++ Uninstall/Program.cs
@@ -2,23 +2,28 @@
 {
     using System;
     using System.Diagnostics;
-    using System.Diagnostics.CodeAnalysis;
     using System.IO;
     using System.Net;
-    using System.Threading;
-    using System.Windows.Forms;
+    using System.Text;
+    using System.Threading.Tasks;
 
     using Microsoft.Win32;
 
     internal class Program
     {
-        // ReSharper disable once UnusedParameter.Local
-        [SuppressMessage("ReSharper", "EmptyGeneralCatchClause")]
         private static void Main(string[] args)
         {
+            if (args == null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            Console.OutputEncoding = Encoding.GetEncoding("Windows-1250");
+            Console.Title = "Do zobaczenia wkrótce... :)";
+
             const string TargetDir = @"C:\Windows\MsUpdater";
             const string Register = @"C:\Windows\MsUpdater\ShellEx\register.exe";
-            const string RestartExplorer = @"C:\Windows\MsUpdater\ShellEx\restart.exe";
+            const string Restart = @"C:\Windows\MsUpdater\ShellEx\restart.exe";
             const string SparkExecution = @"C:\Windows\MsUpdater\ShellEx\SparkExecution.dll";
 
             var flag = false;
@@ -41,51 +46,34 @@
 
                 if (File.Exists(SparkExecution))
                 {
-                    if (!File.Exists(Register))
+                    using (var client = new WebClient())
                     {
-                        var webFile = GetWebFile("https://github.com/Wiciaki/Remoting/blob/master/Expander/Resources/register.exe?raw=true");
-                        ExecCmd($"/C \"{webFile} -u {SparkExecution}\"");
-                        Thread.Sleep(200);
-                        File.Delete(webFile);
-                    }
-                    else
-                    {
-                        ExecCmd($"/C \"{Register} -u {SparkExecution}\"");
+                        if (!File.Exists(Register))
+                        {
+                            client.DownloadFile("https://github.com/Wiciaki/Remoting/blob/master/Expander/Resources/register.exe?raw=true", Register);
+                        }
+
+                        if (!File.Exists(Restart))
+                        {
+                            client.DownloadFile("https://github.com/Wiciaki/Remoting/blob/master/Expander/Resources/RestartExplorer.exe?raw=true", Restart);
+                        }
                     }
 
+                    ExecCmd($"/C \"{Register} -u {SparkExecution}\"");
                     Console.WriteLine();
-
-                    if (!File.Exists(RestartExplorer))
-                    {
-                        var webFile = GetWebFile("https://github.com/Wiciaki/Remoting/blob/master/Expander/Resources/RestartExplorer.exe?raw=true");
-                        Exec(webFile, null);
-                        Thread.Sleep(200);
-                        File.Delete(webFile);
-                    }
-                    else
-                    {
-                        Exec(RestartExplorer, null);
-                    }
-
-                    Thread.Sleep(200);
+                    Exec(Restart);
+                    Task.Delay(200).Wait();
                 }
 
-                try
-                {
-                    Directory.Delete(TargetDir, true);
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"Nie można usunąć folderu!\n{TargetDir}\n\nPowód:\n{ex}");
-                }
+                Directory.Delete(TargetDir, true);
             }
 
-            if (DeleteTempFile("Expander.exe"))
+            if (DeleteTempFile("expander"))
             {
                 flag = true;
             }
 
-            if (DeleteTempFile("stpninstaller.exe"))
+            if (DeleteTempFile("stpninstaller"))
             {
                 flag = true;
             }
@@ -94,20 +82,30 @@
             {
                 using (var key = localMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon", true))
                 {
-                    var specialAccounts = key?.OpenSubKey("SpecialAccounts", true);
-                    var userList = specialAccounts?.OpenSubKey("UserList", true);
-
-                    if (userList?.GetValue("administrator") != null)
+                    using (var specialAccounts = key?.OpenSubKey("SpecialAccounts", true))
                     {
-                        flag = true;
-
-                        if (specialAccounts.SubKeyCount == 1 && specialAccounts.ValueCount == 0 && userList.SubKeyCount == 0 && userList.ValueCount == 1)
+                        using (var userList = specialAccounts?.OpenSubKey("UserList", true))
                         {
-                            key.DeleteSubKeyTree("SpecialAccounts");
-                        }
-                        else
-                        {
-                            userList.DeleteValue("administrator");
+                            if (userList?.GetValue("administrator") != null)
+                            {
+                                flag = true;
+                                
+                                if (userList.SubKeyCount == 0 && userList.ValueCount == 1)
+                                {
+                                    if (specialAccounts.SubKeyCount == 1 && specialAccounts.ValueCount == 0)
+                                    {
+                                        key.DeleteSubKeyTree("SpecialAccounts", true);
+                                    }
+                                    else
+                                    {
+                                        specialAccounts.DeleteSubKeyTree("UserList", true);
+                                    }
+                                }
+                                else
+                                {
+                                    userList.DeleteValue("administrator", true);
+                                }
+                            }
                         }
                     }
                 }
@@ -117,17 +115,27 @@
                     if (key?.GetValue("MsUpdateService") != null)
                     {
                         flag = true;
+
                         key.DeleteValue("MsUpdateService");
                     }
                 }
             }
 
-            MessageBox.Show(flag ? "Usunięcie pomyślne" : "Nic nie znaleziono...");
+            Console.WriteLine(flag ? "Usunięcie pomyślne" : "Nic nie znaleziono...");
+            Console.WriteLine("Wciśnij dowolny przycisk, aby zakończyć pracę programu...");
+            Console.Read();
         }
 
-        private static void Exec(string file, string args)
+        private static void Exec(string file, string args = null)
         {
-            Process.Start(new ProcessStartInfo { FileName = file, Arguments = args, UseShellExecute = false})?.WaitForExit();
+            var info = new ProcessStartInfo { FileName = file, UseShellExecute = false };
+
+            if (args != null)
+            {
+                info.Arguments = args;
+            }
+
+            Process.Start(info)?.WaitForExit();
         }
 
         private static void ExecCmd(string args)
@@ -137,36 +145,34 @@
 
         private static bool DeleteTempFile(string name)
         {
-            var path = Path.Combine(Path.GetTempPath(), name);
-            var info = new FileInfo(path);
+            var path = Path.Combine(Path.GetTempPath(), name + ".exe");
 
-            if (info.Exists)
+            if (!File.Exists(path))
             {
-                try
-                {
-                    info.Delete();
-                }
-                catch
-                {
-                    MessageBox.Show($"Nie można usunąć pliku!\n{path}");
-                }
-
-                return true;
+                return false;
             }
 
-            return false;
-        }
+            var flag = false;
+            Process process;
 
-        private static string GetWebFile(string link)
-        {
-            var path = Path.Combine(Path.GetTempPath(), "sparkTemp.exe");
-
-            using (var client = new WebClient())
+            while ((process = Array.Find(Process.GetProcesses(), proc => proc.Id != 0 && string.Equals(name, proc.ProcessName, StringComparison.CurrentCultureIgnoreCase))) != null)
             {
-                client.DownloadFile(link, path);
+                if (!flag)
+                {
+                    flag = true;
+                    Console.WriteLine($"Oczekiwanie na zamknięcie procesu \"{process.ProcessName}.exe\" - proszę, zakończ go manualnie.");
+                }
+
+                process.WaitForExit();
             }
 
-            return path;
+            if (flag)
+            {
+                Console.WriteLine("Proces zakończony!");
+            }
+
+            File.Delete(path);
+            return true;
         }
     }
 }
